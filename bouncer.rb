13746b18465bf90dca049ffe27db8e65e18dbc4a
StefanoPruna <stefano.pruna@googlemail.com>
2021-04-26 12:02:30 +1000

    Created a small program with if statement

---
+++ bouncer.rb
@@ -0,0 +1,15 @@
+#Check if the person is older than 18 yo and not drunk
+puts "How old are you?"
+yourAge = gets.strip
+yourAge = yourAge.to_i
+
+#yourStatus = true
+puts "Are you drunk? (yes or no)"
+yourStatus = gets.strip
+if yourStatus == "yes"
+    yourStatus = true
+else
+    yourStatus = false
+end
+
+puts !yourStatus && yourAge > 17 ? "You can get in" : "You are not allowed in!"

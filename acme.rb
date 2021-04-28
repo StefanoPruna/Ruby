d16d6e64b5cddf89a5972d324edd06a5dfa94af4
StefanoPruna <stefano.pruna@googlemail.com>
2021-04-28 18:46:43 +1000

    more hash examples and keep going with the test answer

--- acme.rb
+++ acme.rb
@@ -1,33 +1,16 @@
-codingSkills = {:Python => 0, :Ruby => 0, :Bash => 0, :Git => 0, :HTML => 0, :TDD => 0, :CSS => 0, :JavaScript => 0}
-for i, x in codingSkills
-    puts "What's your coding skills for #{i} (1 to 10):"
-    overalSkill = gets.strip.to_i
+
+def yourSkill()
+    overalSkill = []
+    codingSkills = ["Python", "Ruby", "Bash", "Git", "HTML", "TDD", "CSS", "JavaScript"]
+    for i in codingSkills
+        puts "What's your coding skills for #{i} (1 to 10):"
+        skill = gets.chomp.to_i
+        overalSkill.push skill
+    end
+    print overalSkill
+    # overalSkill = skill / 8
+    # overalSkill.each {|skills| puts "Your overall coding skills is: #{skills} #{skill}"}
+    # # puts "Your overall coding skills is: #{i} #{overalSkill}"
 end
-codingSkills.each do |overalSkill|
-    puts "Your overall coding skills is: #{overalSkill}"
-end
 
-
-# def mySkills()
-#     puts "What's your coding skills for Python:"
-#     python = gets.strip.to_i
-#     puts "What's your coding skills for Ruby:"
-#     ruby = gets.strip.to_i
-#     puts "What's your coding skills for Bash:"
-#     bash = gets.strip.to_i
-#     puts "What's your coding skills for Git:"
-#     git = gets.strip.to_i
-#     puts "What's your coding skills for HTML:"
-#     html = gets.strip.to_i
-#     puts "What's your coding skills for TDD:"
-#     tdd = gets.strip.to_i
-#     puts "What's your coding skills for CSS:"
-#     css = gets.strip.to_i
-#     puts "What's your coding skills for JavaScript:"
-#     javascript = gets.strip.to_i
-#     codingSkills = [python, ruby, bash, git, html, tdd, css, javascript].count
-#     overall = codingSkills
-#     puts "your overall is : #{overall}"
-# end 
-
-# mySkills()
+yourSkill()
--- hash.rb
+++ hash.rb
@@ -1,12 +1,37 @@
-fruits = {"banana" => 2, "orange" => 2.5, "avocado" => 2.45}
+# fruits = {"banana" => 2, "orange" => 2.5, "avocado" => 2.45}
 
-veggies = {:carrot => 1.75, :celery => 2.75, :kale => 4}
+# veggies = {:carrot => 1.75, :celery => 2.75, :kale => 4}
 
-# Your code here
-fruits = {:banana => 2, :orange => 2.5, :avocado => 2.45, :peach => 5}
-fruits[:avocado] = 3
-fruits.delete(:orange)
-inventory = fruits.merge!(veggies)
-for i, x in inventory
-    puts "Item: #{i} Cost: $#{x}"
+# # Your code here
+# fruits = {:banana => 2, :orange => 2.5, :avocado => 2.45, :peach => 5}
+# fruits[:avocado] = 3
+# fruits.delete(:orange)
+# inventory = fruits.merge!(veggies)
+# for i, x in inventory
+#     puts "Item: #{i} Cost: $#{x}"
+# end
+
+def full_family
+    family = { 
+        aunts: ["Jane", "Jill" , "Beth"], 
+        sisters: ["Mary", "Sally", "Susan"], 
+        brothers: [ "Steve", "Bob", "Joe"], 
+        uncles: ["Frank", "Rob", "David"] 
+    }
+    # Don't change the code above
+
+
+    # Part 1: Your code here, start by changing immediate_family
+    immediate_family = family[:sisters] + family[:brothers]
+    print immediate_family
+
+    # Part 2: Make changes to family here
+    family[:uncles].push("Jacob")
+    family[:aunts].delete("Jill")
+    puts family
+
+    # Don't change the code below
+    return [family, immediate_family]
 end
+
+full_family()

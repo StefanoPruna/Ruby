cae63596e37ababf80d7315a7237dae0764ad4a2
StefanoPruna <48112000+StefanoPruna@users.noreply.github.com>
2021-04-30 17:55:13 +1000

    Iterators and Method

--- iterators.rb
+++ iterators.rb
@@ -4,12 +4,24 @@ def count_letters(input)
 	letter_count = {}
 	# Your code here
 	# Populate letter_count using an iterator 
-	letter_count.each do |input, count|
-        
-        puts letter_count(input, count)
-    end
-	return letter_count
 end
 
-# Outputting the return value of count_letters
-puts count_letters("Hello World")
+# # Outputting the return value of count_letters
+# puts count_letters("Hello World")
+
+# def book_keeper
+#     books_array = []
+#     # Your code here, start by adding some books to the books_array
+#     books_array = {
+# 		title: ["Harry Potter", "Dune", "Batman"],
+#         author: ["by J.K.Rowling", "by Mark", "by DC Comics"],
+#         year: ["(1997)", "(1970)", "(1934)"]
+# 	}
+# 	#books_array.each {|value| puts "#{key} #{value}"}
+# 	# books_array = books[:title] + books[:author] + books[:year]
+# 	books_array.each 
+# 	return books_array
+# end
+
+# Outputting the return value of the book_keeper method
+# puts book_keeper
---
+++ method.rb
@@ -0,0 +1,22 @@
+def print_greeting(name)
+    puts "What's your name?"
+    name = gets.strip
+    puts "Hi, my name is #{name}"
+end
+
+print_greeting("ste")
+
+def add_three(number)
+    puts "Inser a number"
+    number = gets.strip.to_i
+    return number+3
+end
+
+add_three(3)
+
+def get_last_two_names(names)
+    names = ["sara", "james", "ste"]
+    print names[-2..-1]
+end
+
+get_last_two_names("ste")

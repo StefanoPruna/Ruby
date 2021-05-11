775d9c97d80ebb405a01e550749ed1f16716a7dc
StefanoPruna <stefano.pruna@googlemail.com>
2021-05-11 17:09:41 +1000

    try to pring the highest and the lowest number in a string, but failed

---
+++ maxAndMin.rb
@@ -0,0 +1,14 @@
+def max_min_in_string (str)
+    # Your code here
+    maxAndMin = []
+    #str.split
+    maxAndMin.push(str.split)
+    maxAndMin.to_s      
+    maxAndMin.each do |h|
+        if  > 
+            
+        end
+    end
+end
+
+max_min_in_string("5 2 9 8 23 1")
